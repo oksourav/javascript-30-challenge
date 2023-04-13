708b5cc0af8a3a4b604801bb27155268d3616b2d
boulevard-of-broken-dreams <dash.sourav126@gmail.com>
2023-04-13 19:02:59 +0530

    added readme.md

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,31 +1,33 @@
-window.addEventListener('load',function() {
-	const selector = document.querySelector('.wrapper');
-	let hue = 10;
-	setInterval(() =>{
-		let colorValue = `hsl(${hue},100%,50%)`;
-		selector.style.background = colorValue;
-		hue += 10;
-	},400);
+window.addEventListener("load", function () {
+  const selector = document.querySelector(".wrapper");
+  let hue = 10;
+  setInterval(() => {
+    let colorValue = `hsl(${hue},100%,50%)`;
+    selector.style.background = colorValue;
+    hue += 10;
+  }, 400);
 
-	const checkBoxes = document.querySelectorAll('input');
+  const checkBoxes = document.querySelectorAll("input");
 
-	let inBetween =false;
-	let previous;
+  let inBetween = false;
+  let previous;
 
-	function handleMultiple(e) {
-		if (e.shiftKey && this.checked) {
-			checkBoxes.forEach( (checkbox) => {
-				if (checkbox == this || checkbox == previous) {
-					inBetween = !inBetween;
-				}
+  function handleMultiple(e) {
+    if (e.shiftKey && this.checked) {
+      checkBoxes.forEach((checkbox) => {
+        if (checkbox == this || checkbox == previous) {
+          inBetween = !inBetween;
+        }
 
-				if (inBetween) {
-					checkbox.checked = true;
-				}
-			});
-		}
-		previous = this;
-	}
+        if (inBetween) {
+          checkbox.checked = true;
+        }
+      });
+    }
+    previous = this;
+  }
 
-	checkBoxes.forEach( checkbox => checkbox.addEventListener('click',handleMultiple) );
+  checkBoxes.forEach((checkbox) =>
+    checkbox.addEventListener("click", handleMultiple)
+  );
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+This Repo contains https://javascript30.com/ course exercise code.
+
+Code was written on October 23 2017.
